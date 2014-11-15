315a54fcfa0b10cad96b541e1be3265bb7b94cee
Jiaying Wei <jwei@jwei-Latitude-E6430.(none)>
2014-11-14 23:35:17 -0500

    finished turning a music file into a working function

--- cleaninfinity.py
+++ cleaninfinity.py
@@ -1,19 +1,24 @@
 import math, numpy, wave
 import matplotlib.pyplot as plt
 from pylab import fromstring
+from math import pi 
 
 
-def inputsignal(t):
-	return math.sin(t)
-
+def inputSignal(t,squishedlist, resolution = 10000):
+	"""Finds the value of a signal at time t
+	"""
+	for i in range(int(t*resolution/(2*pi) - 5), int(t*resolution/(2*pi) + 5)):
+		if squishedlist[i][0] >= t:
+			return squishedlist[i][1]
 
 def WAVtoSignal(pathname):
 	"""Takes in a .wav music file and outputs its waveform
 	"""
 	song = wave.open(pathname,'r')
 	waveform = song.readframes(-1)
-	waveform = fromstring(waveform, dtype = int)
-	return waveform
+	waveform = fromstring(waveform, 'Int16')
+	normalized = normSignal(waveform)
+	return normalized
 
 def normSignal(waveform):
 	""" Takes a waveform: normalizes the x to [-1, 1] and normalizes the y to [0, 2*pi]
@@ -21,21 +26,20 @@ def normSignal(waveform):
 	resolution = 10000					#points of resolution
 	step = len(waveform)/ resolution	#step size
 	squished = []						#squished waveform
-	minimum = min(waveform)
-	maximum = max(waveform)
+	minimum = float(min(waveform))
+	maximum = float(max(waveform))
 	for i in range(resolution):
-		squished.append(waveform[i * step])
-		squished[i] = remapInterval(squished[i], minimum, maximum, -1, 1)
-	plt.plot(squished)
-	plt.show()
+		squished.append([i*2*pi/resolution, float(waveform[i * step])])
+		squished[i][1] = remap(squished[i][1], minimum, maximum, -1, 1)
 	return squished
 
 
-def remapInterval(value, input_start, input_end, output_start, output_end):
+def remap(value, low1, high1, low2, high2):
 	""" Maps the input value that is in the interval [input_interval_start, input_interval_end]
 		to the output interval [output_interval_start, output_interval_end].
 	"""
-	return (value - input_start) * (output_end - output_start) / (input_end - input_start) + output_start
+	return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
+	# return value - low1
 
 
 def fourierTransform(input_signal, depth):
@@ -50,9 +54,9 @@ def calculateCurve(amplitude, phi, t):
 	""" Takes in the Amplitude and Phi information of the Fourier transform of the input signal
 		and outputs the value of the calculated curve at a time t
 	"""
-	return 0 + L/2*math.pi
+	return 0 + L/2*pi
 
-def plotAgainstT(function, trange = 2 * math.pi):
+def plotAgainstT(function, trange = 2 * pi):
 	""" Takes in an input function and plots it against time. Use it for debugging
 	"""
 	x = []
@@ -65,7 +69,7 @@ def plotAgainstT(function, trange = 2 * math.pi):
 	plt.show()
 
 
-def plotParametric(function1, function2, trange = 2 * math.pi):
+def plotParametric(function1, function2, trange = 2 * pi):
 	""" Takes in an input function and plots it parametrically against SOMETHING
 	"""
 	x = []
@@ -80,7 +84,9 @@ def plotParametric(function1, function2, trange = 2 * math.pi):
 def main():
 	""" Body runs here
 	"""
-	duck = WAVtoSignal('comebackhome.wav')
-	normSignal(duck)	
+
+	toanyone = WAVtoSignal('comebackhome.wav')
+	print inputSignal(2, toanyone)
+
 if __name__ == '__main__':
 	main()
